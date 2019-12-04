ae2366da94dcab09d76c11f458080dd1af9a6a73
MoniqueCastro <moniquelimacastro@gmail.com>
2019-12-04 12:59:26 +0100

    Updates

---
+++ precache-manifest.b87ec4e7866fdd9c28bb23bd552f2319.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5777b8f88fb6c2cce165434e74d6270",
+    "url": "/star-wars-app/static/media/Starjhol.b5777b8f.woff"
+  },
+  {
+    "revision": "6c384fc4cd79414e663f",
+    "url": "/star-wars-app/static/js/runtime~main.5791dfc2.js"
+  },
+  {
+    "revision": "0ef9e17fbc9636d5d669",
+    "url": "/star-wars-app/static/js/main.2698d23a.chunk.js"
+  },
+  {
+    "revision": "3b6fe904beca4905e315",
+    "url": "/star-wars-app/static/js/2.a94d1e15.chunk.js"
+  },
+  {
+    "revision": "0ef9e17fbc9636d5d669",
+    "url": "/star-wars-app/static/css/main.218315db.chunk.css"
+  },
+  {
+    "revision": "3b6fe904beca4905e315",
+    "url": "/star-wars-app/static/css/2.93ae9b7e.chunk.css"
+  },
+  {
+    "revision": "a2d782e991d15dae053f40ac1c186680",
+    "url": "/star-wars-app/index.html"
+  }
+];
